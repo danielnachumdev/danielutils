6d6954dc87683b8eaf6492fa4a166396d6b5b0de
danielnachumdev <danielnachumdev@gmail.com>
2022-10-08 02:58:21 +0300

    added testing utilities

--- tmp.py
+++ tmp.py
@@ -1,5 +1,19 @@
+from danielutils.Exceptions import ValidationTypeError
+from danielutils import TestFactory, Test, validate
+from typing import Sequence, Union, Any
 
-import danielutils
 
-danielutils.write_to_file(
-    "dir.txt", [f"{v}\n" for v in dir(danielutils) if "__" not in v])
+@validate(int, int)
+def add(x, y):
+    return x+y
+
+
+tester = TestFactory(add)
+tester.add_tests([
+    Test((1, 2), 3),
+    Test((3, 2), 3,),
+    Test((1, 2), 3),
+    Test((1, 2.0), 3),
+    Test((1, 2.5), 3, ValidationTypeError),
+])
+tester()
