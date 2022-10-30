b29bb9ec69313da9d85a277b61933812498ae7b7
danielnachumdev <danielnachumdev@gmail.com>
2022-10-31 00:47:36 +0200

    removed files from commit 2

--- tests/test_system.py
+++
@@ -1,8 +0,0 @@
-from ..danielutils import TestFactory, Test
-from ..danielutils.System import *
-
-
-def test_cm():
-    assert TestFactory(cm).add_tests([
-
-    ])()
--- tests/test_threading.py
+++
@@ -1,8 +0,0 @@
-from ..danielutils import TestFactory, Test
-from ..danielutils.Threading import *
-
-
-def test_atomic_print():
-    assert TestFactory(atomic_print).add_tests([
-
-    ])()
