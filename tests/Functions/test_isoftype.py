d662677d0ac8e416ab3ba832efd636752d3d4067
danielnachumdev <danielnachumdev@gmail.com>
2023-04-20 23:47:33 +0300

    changed '==' to 'is'

--- tests/Functions/test_isoftype.py
+++ tests/Functions/test_isoftype.py
@@ -1,29 +1,29 @@
-from ...danielutils.Functions import isoftype
 from typing import *
+from ...danielutils.Functions import isoftype
 
 
 def test_primitives():
-    assert isoftype(5, int) == True
-    assert isoftype('hello', int) == False
-    assert isoftype(5, Union[str, int]) == True
-    assert isoftype(True, Union[str, float, type]) == False
-    assert isoftype(5, int) == True
-    assert isoftype(5, float) == False
-    assert isoftype("", str) == True
-    assert isoftype([""], list) == True
-    assert isoftype([""], list[str]) == True
-    assert isoftype([""], list[int]) == False
-    assert isoftype(1, int) == True
-    assert isoftype("hello", str) == True
-    assert isoftype([1, 2, 3], list) == True
-    assert isoftype([1, 2, 3], list[int]) == True
-    assert isoftype({1: "a", 2: "b"}, dict) == True
-    assert isoftype({1: "a", 2: "b"}, dict[int, str]) == True
-    assert isoftype(1, float) == False
-    assert isoftype("hello", int) == False
-    assert isoftype([1, 2, 3], list[str]) == False
-    assert isoftype([1, 2, "3"], list[int]) == False
-    assert isoftype({1: "a", 2: "b"}, dict[str, int]) == False
+    assert isoftype(5, int) is True
+    assert isoftype('hello', int) is False
+    assert isoftype(5, Union[str, int]) is True
+    assert isoftype(True, Union[str, float, type]) is False
+    assert isoftype(5, int) is True
+    assert isoftype(5, float) is False
+    assert isoftype("", str) is True
+    assert isoftype([""], list) is True
+    assert isoftype([""], list[str]) is True
+    assert isoftype([""], list[int]) is False
+    assert isoftype(1, int) is True
+    assert isoftype("hello", str) is True
+    assert isoftype([1, 2, 3], list) is True
+    assert isoftype([1, 2, 3], list[int]) is True
+    assert isoftype({1: "a", 2: "b"}, dict) is True
+    assert isoftype({1: "a", 2: "b"}, dict[int, str]) is True
+    assert isoftype(1, float) is False
+    assert isoftype("hello", int) is False
+    assert isoftype([1, 2, 3], list[str]) is False
+    assert isoftype([1, 2, "3"], list[int]) is False
+    assert isoftype({1: "a", 2: "b"}, dict[str, int]) is False
 
 
 def test_advanced_types():
@@ -31,24 +31,24 @@ def test_advanced_types():
     d[int] = 0
     d["str"] = str
 
-    assert isoftype("hello", Any) == True
-    assert isoftype([1, 2, "3"], list[Union[int, str]]) == True
-    assert isoftype(Union, type(Union)) == True
-    assert isoftype(Union[int, float], type(Union)) == True
-    assert isoftype(Union, type(Union)) == True
-    assert isoftype(1, Union[int, float]) == True
-    assert isoftype(int, Union[int, float, type]) == True
-    assert isoftype(int, [int, float, type]) == True
-    assert isoftype(1, Union[int, list[int]]) == True
-    assert isoftype([4], Union[int, list[int]]) == True
-    assert isoftype([4.5], Union[int, list[int]]) == False
-    assert isoftype([5, 6], list[int]) == True
-    assert isoftype([5, 6], list[Union[int, float]]) == True
-    assert isoftype([5, 6.3], list[Union[int, float]]) == True
-    assert isoftype([5.0, 6.3], list[Union[int, float]]) == True
-    assert isoftype(dict(one=1), dict[str, int]) == True
-    assert isoftype(d, dict[Union[type, str], Any]) == True
-    assert isoftype(d, dict) == True
+    assert isoftype("hello", Any) is True
+    assert isoftype([1, 2, "3"], list[Union[int, str]]) is True
+    assert isoftype(Union, type(Union)) is True
+    assert isoftype(Union[int, float], type(Union)) is True
+    assert isoftype(Union, type(Union)) is True
+    assert isoftype(1, Union[int, float]) is True
+    assert isoftype(int, Union[int, float, type]) is True
+    assert isoftype(int, [int, float, type]) is True
+    assert isoftype(1, Union[int, list[int]]) is True
+    assert isoftype([4], Union[int, list[int]]) is True
+    assert isoftype([4.5], Union[int, list[int]]) is False
+    assert isoftype([5, 6], list[int]) is True
+    assert isoftype([5, 6], list[Union[int, float]]) is True
+    assert isoftype([5, 6.3], list[Union[int, float]]) is True
+    assert isoftype([5.0, 6.3], list[Union[int, float]]) is True
+    assert isoftype(dict(one=1), dict[str, int]) is True
+    assert isoftype(d, dict[Union[type, str], Any]) is True
+    assert isoftype(d, dict) is True
 
 
 def test_classes():
@@ -61,24 +61,24 @@ def test_classes():
     class MyOtherClass(MyClass):
         pass
 
-    assert isoftype(MyChildClass(), MyClass) == True
-    assert isoftype(MyChildClass(), MyChildClass) == True
-    assert isoftype(MyChildClass(), MyOtherClass) == False
+    assert isoftype(MyChildClass(), MyClass) is True
+    assert isoftype(MyChildClass(), MyChildClass) is True
+    assert isoftype(MyChildClass(), MyOtherClass) is False
 
 
 def test_callable():
-    assert isoftype(lambda x: x+1, Callable[[int], int]) == False
-    assert isoftype(lambda x: x+1, Callable[[float], int]) == False
-    assert isoftype(lambda x: x+1, Callable[[int], Union[int, str]]) == False
-    assert isoftype(lambda x: x+1, Callable) == False
-    assert isoftype(lambda x: x+1, Callable[[int], tuple[int, str]]) == False
-    assert isoftype(lambda x: x+1, Callable, strict=False) == True
+    assert isoftype(lambda x: x+1, Callable[[int], int]) is False
+    assert isoftype(lambda x: x+1, Callable[[float], int]) is False
+    assert isoftype(lambda x: x+1, Callable[[int], Union[int, str]]) is False
+    assert isoftype(lambda x: x+1, Callable) is False
+    assert isoftype(lambda x: x+1, Callable[[int], tuple[int, str]]) is False
+    assert isoftype(lambda x: x+1, Callable, strict=False) is True
 
     def foo(a: int) -> int:
         a += 1
-    assert isoftype(foo, Callable) == True
-    assert isoftype(foo, Callable[[int], int]) == True
-    assert isoftype(Callable, type(Callable)) == True
-    assert isoftype(Callable[[], bool], type(Callable[[], bool])) == True
+    assert isoftype(foo, Callable) is True
+    assert isoftype(foo, Callable[[int], int]) is True
+    assert isoftype(Callable, type(Callable)) is True
+    assert isoftype(Callable[[], bool], type(Callable[[], bool])) is True
     assert isoftype(Callable[[int, float], bool], type(
-        Callable[[int, float], bool])) == True
+        Callable[[int, float], bool])) is True
