f7d187d657baf8aebf00d6f4f08e7e8568e26e51
danielnachumdev-hplaptop <danielnachumdev@gmail.com>
2023-05-10 11:33:33 +0300

    simplified join_generators

--- danielutils/Generators/join_generators.py
+++ danielutils/Generators/join_generators.py
@@ -1,3 +1,4 @@
+import threading
 from typing import Generator, Any
 from ..Decorators import threadify
 from ..MetaClasses import AtomicClassMeta
@@ -14,22 +15,32 @@ def join_generators(*generators) -> Generator[Any, None, None]:
     Yields:
         Generator[Any, None, None]: resulting generator
     """
+    q_output_sem = threading.Semaphore(0)
+    q_input_sem = threading.Semaphore(1)
     q = AtomicQueue()
     threads_status = [False for _ in range(len(generators))]
 
     @threadify
     def yield_from_one(thread_id: int, gen):
         nonlocal threads_status
-        try:
-            while True:
-                q.push(next(gen))
-        except StopIteration:
-            threads_status[thread_id] = True
+        # try:
+        for v in gen:
+            q.push(v)
+        threads_status[thread_id] = True
+        #     while True:
+        #         q_input_sem.acquire()
+        #         q.push(next(gen))
+        #         q_input_sem.release()
+        #         q_output_sem.release()
+        # except StopIteration:
+        #     threads_status[thread_id] = True
 
     for i, gen in enumerate(generators):
         yield_from_one(i, gen)
 
+    # busy waiting
     while not all(threads_status):
+        # q_output_sem.acquire()
         while not q.is_empty():
             yield q.pop()
     if not q.is_empty():
