d2dfbffecc968f14c7a6305881d04188c6412dce
danielnachumdev <danielnachumdev@gmail.com>
2023-06-02 02:33:39 +0300

    fixed join_generator tests

--- tests/Generators/test_join_generators.py
+++ tests/Generators/test_join_generators.py
@@ -21,7 +21,8 @@ def test_simple_case():
     for v in join_generators_busy_waiting(gen1(), gen2()):
         res.append(v)
 
-    assert res == [0, 1, 2, 3, 0, 4, 5, 1, 6, 2, 7, 3, 8, 4, 5, 9, 6, 7, 8, 9]
+    assert res == [(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (0, 4), (0, 5), (1, 1), (0, 6),
+                   (1, 2), (0, 7), (1, 3), (0, 8), (1, 4), (1, 5), (0, 9), (1, 6), (1, 7), (1, 8), (1, 9)]
 
 
 def test_simple_case2():
@@ -41,5 +42,6 @@ def test_simple_case2():
     res = []
     for v in join_generators(gen1(), gen2()):
         res.append(v)
-
-    assert res == [0, 1, 2, 3, 0, 4, 5, 1, 6, 2, 7, 3, 8, 4, 5, 9, 6, 7, 8, 9]
+    print(res)
+    assert res == [(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (0, 4), (0, 5), (1, 1), (0, 6),
+                   (1, 2), (0, 7), (1, 3), (0, 8), (1, 4), (1, 5), (0, 9), (1, 6), (1, 7), (1, 8), (1, 9)]
