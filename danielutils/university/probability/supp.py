7ead6c39a22a54f316194ed3f95d4037f5f57f1f
danielnachumdev <danielnachumdev@gmail.com>
2024-04-17 21:49:03 +0300

    added properties to Supp
    Took 5 minutes

--- danielutils/university/probability/supp.py
+++ danielutils/university/probability/supp.py
@@ -1,13 +1,26 @@
+from abc import abstractmethod, ABC
 from typing import Iterator, TypeVar, Union
 from ...classes import frange
 
 T = TypeVar('T')
 
 
-class Supp(Iterator[T]): ...
+class Supp(ABC, Iterator[T]):
+    @property
+    @abstractmethod
+    def is_finite(self) -> bool: ...
+
+    def is_infinite(self) -> bool:
+        return not self.is_finite
 
 
 class DiscreteSupp(Supp[int]):
+    @property
+    def is_finite(self) -> bool:
+        if isinstance(self._r, frange):
+            return self._r.is_finite
+        return True
+
     def __next__(self):
         yield from self
 
@@ -18,7 +31,11 @@ class DiscreteSupp(Supp[int]):
         return iter(self._r)
 
 
-class ContinuseSupp(Supp[float]): ...
+class ContinuseSupp(Supp[float]):
+
+    @property
+    def is_finite(self) -> bool:
+        return False
 
 
 __all__ = [
