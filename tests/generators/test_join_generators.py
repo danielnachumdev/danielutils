2b0572bc8922dae2495e1232a1ecf06304fe11c1
danielnachumdev <danielnachumdev@gmail.com>
2024-04-21 22:28:29 +0300

    small tidyup
    Took 5 minutes

--- tests/generators/test_join_generators.py
+++ tests/generators/test_join_generators.py
@@ -3,8 +3,16 @@ from typing import Generator
 import time
 from danielutils.generators.join_generators import join_generators, join_generators_busy_waiting  # type:ignore
 
+EXPECTED = [(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (0, 4), (0, 5), (1, 1), (0, 6),
+            (1, 2), (0, 7), (1, 3), (0, 8), (1, 4), (1, 5), (0, 9), (1, 6), (1, 7), (1, 8),
+            (1, 9)]
+
 
 class TestJoinGenerators(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        import random
+        random.seed(17)
 
     def test_simple_case(self):
         # if this value is too low the result is not deterministic
@@ -26,9 +34,7 @@ class TestJoinGenerators(unittest.TestCase):
         for v in join_generators_busy_waiting(gen1(), gen2()):
             res.append(v)
 
-        self.assertListEqual(res, [(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (0, 4), (0, 5), (1, 1), (0, 6),
-                                   (1, 2), (0, 7), (1, 3), (0, 8), (1, 4), (1, 5), (0, 9), (1, 6), (1, 7), (1, 8),
-                                   (1, 9)])
+        self.assertListEqual(EXPECTED, res)
 
     def test_simple_case2(self):
         MAX_DURATION = 0.5
@@ -49,6 +55,4 @@ class TestJoinGenerators(unittest.TestCase):
         for v in join_generators(gen1(), gen2()):
             res.append(v)
 
-        self.assertListEqual(res, [(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (0, 4), (0, 5), (1, 1), (0, 6),
-                                   (1, 2), (0, 7), (1, 3), (0, 8), (1, 4), (1, 5), (0, 9), (1, 6), (1, 7), (1, 8),
-                                   (1, 9)])
+        self.assertListEqual(EXPECTED, res)
