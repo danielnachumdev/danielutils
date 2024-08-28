89f634c9c847a880dd2a4b49e0b781fd2474f9d1
danielnachumdev <danielnachumdev@gmail.com>
2024-08-28 09:37:30 +0300

    implemented singleton.py
    Took 2 minutes

--- danielutils/decorators/singleton.py
+++ danielutils/decorators/singleton.py
@@ -1,5 +1,7 @@
 def singleton(og_class):
     instance = None
+    original_new = getattr(og_class, '__new__')
+    original_init = getattr(og_class, '__init__')
 
     def __new__(cls, *args, **kwargs):
         nonlocal instance
@@ -11,11 +13,26 @@ def singleton(og_class):
             blacklist = {*cls.__mro__[:og_index + 1]}
             for candidate in cls.__mro__[og_index + 1:]:
                 if candidate not in blacklist:
-                    instance = candidate.__new__(cls, *args, **kwargs)
-                    break
+                    try:
+                        instance = candidate.__new__(cls, *args, **kwargs)
+                        break
+                    except:
+                        pass
+            else:
+                instance = object.__new__(cls)
         return instance
 
+    is_init: bool = False
+
+    def __init__(self, *args, **kwargs) -> None:
+        nonlocal is_init
+        if not is_init:
+            original_init(self, *args, **kwargs)
+            is_init = True
+
     setattr(og_class, "__new__", __new__)
+    setattr(og_class, "__init__", __init__)
+    setattr(og_class, "instance", lambda: instance)
     return og_class
 
 
