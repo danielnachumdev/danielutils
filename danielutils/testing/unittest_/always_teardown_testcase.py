460729aeba6fea8f34a15fa1843ce37fb38d08b7
danielnachumdev <danielnachumdev@gmail.com>
2024-08-28 16:22:30 +0300

    updated specialized unittest test cases
    Took 10 minutes

--- danielutils/testing/unittest_/always_teardown_testcase.py
+++ danielutils/testing/unittest_/always_teardown_testcase.py
@@ -1,6 +1,7 @@
 import functools
 import unittest
-from typing import Optional, Callable
+from _collections_abc import Coroutine
+from typing import Optional, Callable, Type
 from unittest import TestResult
 
 
@@ -19,7 +20,7 @@ class AlwaysTeardownTestCase(unittest.TestCase):
 
         return super().run(result)
 
-    def _cleanup_wrapper(self, method: Callable, exception) -> Callable:
+    def _cleanup_wrapper(self, method: Callable, exception: Type[BaseException]) -> Callable:
         @functools.wraps(method)
         def wrapper(*args, **kwargs):
             try:
@@ -32,6 +33,35 @@ class AlwaysTeardownTestCase(unittest.TestCase):
         return wrapper
 
 
+class AsyncAlwaysTeardownTestCase(unittest.IsolatedAsyncioTestCase):
+    """
+    SafeTestCase makes sure that tearDown / cleanup methods are always run when
+    They should be.
+    """
+
+    def run(self, result=None) -> Optional[TestResult]:
+        test_method = getattr(self, self._testMethodName)
+        wrapped_test = self._cleanup_wrapper(test_method, KeyboardInterrupt)
+        setattr(self, self._testMethodName, wrapped_test)
+
+        self.asyncSetUp = self._cleanup_wrapper(self.asyncSetUp, BaseException)
+
+        return super().run(result)
+
+    def _cleanup_wrapper(self, method: Coroutine, exception: Type[BaseException]) -> Callable:
+        @functools.wraps(method)
+        async def wrapper(*args, **kwargs):
+            try:
+                return await method(*args, **kwargs)
+            except exception:
+                self.tearDown()
+                self.doCleanups()
+                raise
+
+        return wrapper
+
+
 __all__ = [
-    "AlwaysTeardownTestCase"
+    "AlwaysTeardownTestCase",
+    "AsyncAlwaysTeardownTestCase",
 ]
