e29490d054d1127985c49e05ac095a759a694924
danielnachumdev <danielnachumdev@gmail.com>
2024-08-28 09:50:08 +0300

    fixed the backoff strategies. added more strategies
    Took 12 minutes

--- danielutils/retry_executor/backoff_strategies/exponential_backoff.py
+++ danielutils/retry_executor/backoff_strategies/exponential_backoff.py
@@ -2,16 +2,15 @@ from ..backoff_strategy import BackOffStrategy
 
 
 class ExponentialBackOffStrategy(BackOffStrategy):
-    def __init__(self, initial: int, multiplier: float) -> None:
+    def __init__(self, initial: float) -> None:
         if not initial >= 0:
             raise ValueError("initial must be positive")
-        if not multiplier >= 0:
-            raise ValueError("multiplier must be positive")
-        prev: float = 1.0
+        attempt: int = 1
 
-        def inner() -> int:
-            nonlocal prev
-            return initial ** (prev := prev * multiplier)
+        def inner() -> float:
+            nonlocal attempt
+            attempt += 1
+            return initial ** (attempt - 1)
 
         super().__init__(inner)
 
