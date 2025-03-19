393e645ffe9eeccf27a6d97520be3baf2aa8d241
danielnachumdev <danielnachumdev@gmail.com>
2025-03-20 00:58:54 +0200

    updated to version 1.0.38
    Took 4 minutes

--- danielutils/java/__init__.py
+++ danielutils/java/__init__.py
@@ -1,7 +1,7 @@
 try:
-    from reflection import get_python_version
-except ImportError:
     from ..reflection import get_python_version  # type:ignore
+except ImportError:
+    from reflection import get_python_version
 
 if get_python_version() >= (3, 10):
     from .interfaces import *
