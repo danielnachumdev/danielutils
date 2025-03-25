9a09835d3a912cd82b02a6348b8e146ed1b6d951
danielnachumdev <danielnachumdev@gmail.com>
2025-03-25 17:34:59 +0200

    made names unique
    Took 9 minutes

--- danielutils/testing/unittest_/auto_cwd_testcase.py
+++ danielutils/testing/unittest_/auto_cwd_testcase.py
@@ -5,7 +5,8 @@ import unittest
 from inspect import iscoroutinefunction
 from typing import Callable, Type, Coroutine, Union, Any
 
-from ...io_ import create_directory, delete_directory
+from ...random_ import RandomDataGenerator
+from ...io_ import create_directory, delete_directory, directory_exists
 from ...path import get_current_working_directory, set_current_working_directory
 
 
@@ -18,10 +19,17 @@ def dispatch_function(func: Union[Callable, Coroutine], *args, **kwargs) -> Any:
         return func(*args, **kwargs)
 
 
+def get_available_folder_name(prefix_path: str, random_suffix_length: int = 15) -> str:
+    res = f"{prefix_path}_{RandomDataGenerator.name(random_suffix_length)}"
+    while directory_exists(res):
+        res = f"{prefix_path}_{RandomDataGenerator.name(random_suffix_length)}"
+    return res
+
+
 def improved_setup(func: Callable) -> Callable:
     @functools.wraps(func)
     def wrapper(self):
-        self.cwd = f"./{self.__class__.__name__}_test_folder"
+        self.cwd = get_available_folder_name(f"./{self.__class__.__name__}_test_folder")
         create_directory(self.cwd)
         self.prev_cwd = get_current_working_directory()
         set_current_working_directory(os.path.join(self.prev_cwd, self.cwd))
@@ -30,7 +38,7 @@ def improved_setup(func: Callable) -> Callable:
 
     @functools.wraps(func)
     async def async_wrapper(self):
-        self.cwd = f"./{self.__class__.__name__}_test_folder"
+        self.cwd = get_available_folder_name(f"./{self.__class__.__name__}_test_folder")
         create_directory(self.cwd)
         self.prev_cwd = get_current_working_directory()
         set_current_working_directory(os.path.join(self.prev_cwd, self.cwd))
